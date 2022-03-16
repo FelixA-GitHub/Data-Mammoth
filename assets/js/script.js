fd0b269c8e8ca6900e40335d600845cb96bd2b32
Your Name <felix.acevedo.tech@gmail.com>
2022-03-15 21:03:27 -0400

    movie rating function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,18 @@ var movieTitle = document.getElementById("movietitle");
 var movieDesc = document.getElementById("moviedesc");
 var releaseDate = document.getElementById("releasedate");
 var movieScore = document.getElementById("moviescore");
+var movieRating = document.getElementById("movierating");
 
-//variable for API Key
-var apiKey = "6a29d29b99eb578408eebe9fd0b98eb6"
+//variable for API Keys
+var apiKey1 = "6a29d29b99eb578408eebe9fd0b98eb6";
+var apiKey2 = "k_b81pzrt6";
+
 
 var movieId = "";
 
 // function to pull data from API
 function submitPull (input) {
-    var url = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&query=" + input + "&page=1&include_adult=false";
+    var url = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey1 + "&language=en-US&query=" + input + "&page=1&include_adult=false";
 
     fetch(url)
         .then(function (response) {
@@ -33,6 +36,21 @@ function submitPull (input) {
         });
 }
 
+// function to pull movie rating from API
+function ratingPull (input) {
+    var url2 = "https://imdb-api.com/API/AdvancedSearch/" + apiKey2 + "?title=" + input + "&title_type=feature&certificates=?";
+
+    fetch(url2)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+    var currentRating = data.results[0];
+    console.log(currentRating);
+    movieRating.textContent = currentRating.contentRating;
+    });
+}
+
 // function to handle starting a search
 function submitSearchQuery (event) {
     event.preventDefault();
@@ -40,7 +58,7 @@ function submitSearchQuery (event) {
     var search = searchField.value.trim();
 
     if (search) {
-        submitPull(search);
+        ratingPull(search);
     } else {
         alert("Please enter a title.");
     }
